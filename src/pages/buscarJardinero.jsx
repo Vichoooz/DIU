cb6c4b8f582a2403b3884d7bb20a08bba677b403
Vicente <vichomr2001@gmail.com>
2023-11-16 19:01:24 -0300

    ultimos cambios

--- src/pages/buscarJardinero.jsx
+++ src/pages/buscarJardinero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import NavBar from '../components/nav_bar';
-import { TextField, Button, Select, MenuItem } from '@mui/material';
+import { TextField, Button, Select, MenuItem, Snackbar } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
 
 const centerStyle = {
   display: 'flex',
@@ -11,6 +12,8 @@ const centerStyle = {
 };
 
 const BuscarPage = () => {
+  const [contratado, setContratado] = useState(false); // Estado para controlar la visibilidad del Snackbar
+
   const jardinero = {
     nombre: 'Jardinero Ejemplo',
     precio: 'Precio Medio',
@@ -30,14 +33,23 @@ const BuscarPage = () => {
 
   const comentarioAleatorio = obtenerComentarioAleatorio();
 
-  const handleClick = () => {
+  const handleClickContratar = () => {
+    setContratado(true); // Establecer el estado a true al hacer clic en Contratar
+  };
+
+  const handleCloseSnackbar = () => {
+    setContratado(false); // Establecer el estado a false al cerrar el Snackbar
+  };
+
+  const handleClickChatear = () => {
+    // Redirigir a la página de chat
     history.push('/chat');
   };
 
   return (
     <div style={centerStyle}>
       <NavBar />
-      <h1>Búsqueda de Servicio</h1>
+      <h1>Servicio</h1>
 
       <div>
         <h3>Jardinero Disponible:</h3>
@@ -49,13 +61,20 @@ const BuscarPage = () => {
         <h3>Comentario:</h3>
         <p>{comentarioAleatorio}</p>
       </div>
-       <Button color= "inherit" variant="contained" onClick={() => { /* No hacer nada */ }}>
+
+      <Button color="inherit" variant="contained" onClick={handleClickContratar}>
         Contratar
       </Button>
       <br />
-      <Button color= "inherit" variant="contained" onClick={handleClick} href="chat">
+      <Button color="inherit" variant="contained" onClick={handleClickChatear} href="chat">
         Chatear
       </Button>
+
+      <Snackbar open={contratado} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+        <MuiAlert elevation={6} variant="filled" severity="success" onClose={handleCloseSnackbar}>
+          Contratado!
+        </MuiAlert>
+      </Snackbar>
 
       {/* Aquí puedes agregar un componente de Google Maps si lo necesitas */}
     </div>
